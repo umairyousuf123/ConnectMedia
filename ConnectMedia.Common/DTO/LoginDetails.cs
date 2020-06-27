6b58ed50d9fbcb5753f83935d72e4d7d844e6cee
Muhammad Umair <umair.yousuf@aciano.net>
2020-06-27 14:03:20 +0500

    commit changes

---
+++ ConnectMedia.Common/DTO/LoginDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectMedia.Common.DTO
+{
+    public class loginDetails
+    {
+
+        public string Token { get; set; }
+
+        public string UserId { get; set; }
+
+        public string BuildingKey { get; set; }
+    }
+}
